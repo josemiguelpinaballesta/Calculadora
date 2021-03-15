0d363413f37d86413db1b01339311c84b0686b2d
Josemibek <Josemibek@DESKTOP-9UL1MST>
2021-03-15 14:06:13 +0100

    Subida calculadora

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Josemibek
+ */
+public class Calculadora {
+
+    private int num1;
+    private int num2;
+    
+    public Calculadora(int a, int b){
+        
+     num1=a; 
+     
+     num2= b;
+        
+    }
+            
+   public int suma(){
+       
+       int result= num1+num2;
+       
+       return result;
+       
+   }       
+   
+   public int resta(){
+       
+       int result=num1-num2;
+       
+       return result;
+       
+   }
+   
+   public int multiplica(){
+       
+       int result= num1*num2;
+       
+       return result;
+       
+   }
+   
+   public int divide(){
+       
+       int result=num1/num2;
+       
+       return result;
+      
+   }
+           
+           
+            
+    
+}
